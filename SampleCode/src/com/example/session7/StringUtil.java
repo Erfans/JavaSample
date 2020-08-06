7d46ec57c950ca082cecd158b90c8dd7fd8b928d
Erfan Shamabadi <erfan@codekunst.com>
2020-08-06 19:53:15 +0200

    session 7

---
+++ SampleCode/src/com/example/session7/StringUtil.java
@@ -0,0 +1,20 @@
+package com.example.session7;
+
+public class StringUtil {
+    public static void main(String[] args) {
+
+        String name = "Erfan";
+        System.out.println(name);
+
+        // get a char at specific position
+        char charE = name.charAt(0);
+        System.out.println(charE);
+
+        char char3 = name.charAt(3);
+        System.out.println(char3);
+
+        // get length of string
+        int lengthOfName = name.length();
+        System.out.println(lengthOfName);
+    }
+}
